2adb4da8a49b7cbd977dbd40744f6060258c842c
takano1228 <kmm2459334@stu.o-hara.ac.jp>
2025-04-30 11:18:15 +0900

    ログイン

---
+++ src/scoremanager/main/LoginAction.java
@@ -0,0 +1,30 @@
+package scoremanager.main;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Customer;
+import dao.CustomerDAO;
+import tool.Action;
+
+public class LoginAction extends Action{
+	public String execute(
+			HttpServletRequest request, HttpServletResponse response
+			) throws Exception{
+		HttpSession session=request.getSession();
+
+		String id=request.getParameter("id");
+		String password=request.getParameter("password");
+		CustomerDAO dao=new CustomerDAO();
+		Customer customer=dao.search(id,password);
+
+		if (customer!=null){
+			session.setAttribute("customer",customer);
+			return "main.jsp";
+		}
+		return "login-error.jsp";
+	}
+
+}
+
