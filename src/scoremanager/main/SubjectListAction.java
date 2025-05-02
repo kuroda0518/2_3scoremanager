f6f8cfdf9ee9495e4d964c6a06a38d228abc6ba8
djangouser <kmm2459332@stu.o-hara.ac.jp>
2025-05-02 10:38:40 +0900

    修正

--- src/scoremanager/main/SubjectListAction.java
+++ src/scoremanager/main/SubjectListAction.java
@@ -6,21 +6,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import bean.Subject;
+import bean.Teacher;
 import dao.SubjectDao;
 import tool.Action;
 
 public class SubjectListAction extends Action {
-    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        request.setCharacterEncoding("UTF-8");
-        SubjectDao dao = new SubjectDao();
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	    request.setCharacterEncoding("UTF-8");
 
+	    // セッションからログイン中の先生を取得
+	    Teacher user = (Teacher) request.getSession().getAttribute("user");
+	    if (user == null) {
+	        response.sendRedirect("Login.action");
+	        return null;
+	    }
 
-        // 科目一覧
-        List<Subject> subject = dao.selectAll();
+	    String schoolCd = user.getSchoolCd();  // ← ここで学校コード取得！
 
-        // ▼ リクエストに格納
-        request.setAttribute("subject", subject);
+	    SubjectDao dao = new SubjectDao();
+	    List<Subject> subject = dao.selectBySchool(schoolCd);  // ← こっちを使う！
 
-        return "subject_list.jsp";
-    }
+	    request.setAttribute("subject", subject);
+
+	    return "subject_list.jsp";
+	}
+
 }
