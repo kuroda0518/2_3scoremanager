64af1b75d0f8be5a33ede21d40f1f5020421ce41
djangouser <kmm2459332@stu.o-hara.ac.jp>
2025-05-08 10:58:47 +0900

    科目完成かもしれない

--- src/scoremanager/main/SubjectDeleteExecuteAction.java
+++ src/scoremanager/main/SubjectDeleteExecuteAction.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import bean.Subject;
-import bean.Teacher;
 import dao.SubjectDao;
 import tool.Action;
 
@@ -12,21 +11,14 @@ public class SubjectDeleteExecuteAction extends Action {
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
         request.setCharacterEncoding("UTF-8");
 
-        String subcd = request.getParameter("subcd");       // 科目コード（変更不可）
-
-
-
-        // ログイン中の教員から schoolCd を取得
-        Teacher user = (Teacher) request.getSession().getAttribute("loginUser");
-        String schoolCd = user.getSchoolCd();
+        String subcd = request.getParameter("subcd");
 
         Subject subject = new Subject();
         subject.setCd(subcd);
-        subject.setSchoolCd(schoolCd);
 
         SubjectDao dao = new SubjectDao();
         dao.delete(subject);
 
-        return "subject_update_result.jsp";
+        return "subject_delete_done.jsp";
     }
 }
