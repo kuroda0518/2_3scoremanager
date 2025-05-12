84f65976a6c128f32c05fcb1fb4f1e0d4ba0734f
djangouser <gene04koyuki@gmail.com>
2025-05-12 10:30:23 +0900

    なんか消えてた

---
+++ src/bean/TestListStudent.java
@@ -0,0 +1,28 @@
+package bean;
+
+public class TestListStudent {
+    private String entYear;
+    private String classNum;
+    private String studentNo;
+    private String studentName;
+    private Integer firstScore;
+    private Integer secondScore;
+
+    public String getEntYear() { return entYear; }
+    public void setEntYear(String entYear) { this.entYear = entYear; }
+
+    public String getClassNum() { return classNum; }
+    public void setClassNum(String classNum) { this.classNum = classNum; }
+
+    public String getStudentNo() { return studentNo; }
+    public void setStudentNo(String studentNo) { this.studentNo = studentNo; }
+
+    public String getStudentName() { return studentName; }
+    public void setStudentName(String studentName) { this.studentName = studentName; }
+
+    public Integer getFirstScore() { return firstScore; }
+    public void setFirstScore(Integer firstScore) { this.firstScore = firstScore; }
+
+    public Integer getSecondScore() { return secondScore; }
+    public void setSecondScore(Integer secondScore) { this.secondScore = secondScore; }
+}
