25132cc7396a39c83e57e3f25d28e0901a9332bd
djangouser <kmm2459332@stu.o-hara.ac.jp>
2025-05-21 11:44:26 +0900

    学生更新微修正

--- src/scoremanager/main/StudentUpdateAction.java
+++ src/scoremanager/main/StudentUpdateAction.java
@@ -1,9 +1,13 @@
 package scoremanager.main;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import bean.Student;
+import bean.Teacher;
+import dao.ClassNumDao;
 import dao.StudentDao;
 import tool.Action;
 
@@ -14,7 +18,18 @@ public class StudentUpdateAction extends Action {
         StudentDao sDao = new StudentDao();
         Student student = sDao.find(no);
 
+        Teacher user = (Teacher) request.getSession().getAttribute("loginUser");
+        if (user == null) {
+            response.sendRedirect("Login.action");
+            return null;
+        }
+
+        String schoolCd = user.getSchoolCd();
+
         request.setAttribute("student", student);
+        ClassNumDao classNumDao = new ClassNumDao();
+        List<String> classNumList = classNumDao.findAll(schoolCd);
+        request.setAttribute("classNumList", classNumList);
 
         return "student_update.jsp";
     }
